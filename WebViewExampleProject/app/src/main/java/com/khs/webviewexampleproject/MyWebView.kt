7292323f83e45a7e465d9a29b8a720878eb6159f
KHS-PC\pskna <psknal27@gmail.com>
2021-10-27 14:50:24 +0900

    feat. 웹 뷰 예시 프로젝트 서버와 송신할 경우 메서드 추가

--- WebViewExampleProject/app/src/main/java/com/khs/webviewexampleproject/MyWebView.kt
+++ WebViewExampleProject/app/src/main/java/com/khs/webviewexampleproject/MyWebView.kt
@@ -2,9 +2,14 @@ package com.khs.webviewexampleproject
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /*
 * 웹뷰 클래스
@@ -17,9 +22,23 @@ class MyWebView(context: Context, url: String): WebView(context) {
     init {
         settings.apply {
             javaScriptEnabled = true //자바스크립트 허용.
-
+            javaScriptCanOpenWindowsAutomatically = true // 웹 뷰에서 창을 자동으로 열 수 있도록 한다.
             setSupportMultipleWindows(true) //웹뷰에서 새 창을 열 수 있도록 허용하는 메서드.
         }
+
+        //만일 서버와 데이터를 주고받을 일이 있을 경우.
+        addJavascriptInterface(object: Any() {
+            @JavascriptInterface
+            fun getData(data: String) {
+                CoroutineScope(Dispatchers.Default).launch {
+                    //비동기로 데이터를 가져옴.
+
+                    withContext(CoroutineScope(Dispatchers.Main).coroutineContext) {
+                        //데이터 가져온 후 할 일.
+                    }
+                }
+            }
+        }, "{Name}")
 
         webViewClient = object: WebViewClient() {
             override fun shouldOverrideUrlLoading(
