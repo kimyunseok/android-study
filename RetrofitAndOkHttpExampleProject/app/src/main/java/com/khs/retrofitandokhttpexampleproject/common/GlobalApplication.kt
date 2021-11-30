ad36a8aaae8d1dd20e720d166ff3fde4a09917fe
KHS-PC\pskna <psknal27@gmail.com>
2021-11-30 17:32:16 +0900

    fix. 레트로핏 객체 초기화 위치 수정

--- RetrofitAndOkHttpExampleProject/app/src/main/java/com/khs/retrofitandokhttpexampleproject/common/GlobalApplication.kt
+++ RetrofitAndOkHttpExampleProject/app/src/main/java/com/khs/retrofitandokhttpexampleproject/common/GlobalApplication.kt
@@ -22,24 +22,9 @@ class GlobalApplication: Application() {
     companion object {
         lateinit var prefs: MySharedPreferences
 
-        private val BASE_URL = "https://solved.ac/"
-        // 기본 URL에는 기본 주소만 넣어주도록 한다. /api/v3/도 넣어주면 URL에서 인식이 불가능하다.
-
-        //OKHttp 클라이언트
-        //private val okHttpClient = OkHttpClient.Builder() 만일 SSL인증이 필요없다면 이렇게 구현.
-
-        private val okHttpClient = getUnsafeOkHttpClient() // SSL인증서 허용을 위한 OkHttpClient.Builder
-            .connectTimeout(20, TimeUnit.SECONDS)
-            .readTimeout(20, TimeUnit.SECONDS)
-            //.writeTimeout(20, TimeUnit.SECONDS) //쓰는 기능은 현재 프로젝트에 없음.
-            //.addNetworkInterceptor(InterceptorForHeader()) 헤더가 필요 없음.
-            .build()
-
         //Retrofit2
-        val baseService = Retrofit.Builder().baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        lateinit var baseService: Retrofit
+            private set
 
         //Glide URL -> ImageView 데이터바인딩에서 사용하기 위한 메서드
         @BindingAdapter("imageFromUrl")
@@ -54,38 +39,60 @@ class GlobalApplication: Application() {
                 view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_no_image))
             }
         }
-
-        // SSL을 우회하는 OKHttpClient Builder를 생성한다. 모든 SSL 인증서를 허용하게 된다.
-            private fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-
-                }
-
-                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-
-                }
-
-                override fun getAcceptedIssuers(): Array<X509Certificate> {
-                    return arrayOf()
-                }
-            })
-
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, SecureRandom())
-
-            val sslSocketFactory = sslContext.socketFactory
-
-            val builder = OkHttpClient.Builder()
-            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-            builder.hostnameVerifier { hostname, session -> true }
-
-            return builder
-        }
     }
 
     override fun onCreate() {
         super.onCreate()
         prefs = MySharedPreferences(applicationContext)
+
+        baseService = initRetrofitBuilder()
+    }
+
+    private fun initRetrofitBuilder(): Retrofit {
+        val BASE_URL = "https://solved.ac/"
+        // 기본 URL에는 기본 주소만 넣어주도록 한다. /api/v3/도 넣어주면 URL에서 인식이 불가능하다.
+
+        //OKHttp 클라이언트
+        //private val okHttpClient = OkHttpClient.Builder() 만일 SSL인증이 필요없다면 이렇게 구현.
+        val okHttpClient = getUnsafeOkHttpClient() // SSL인증서 허용을 위한 OkHttpClient.Builder
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            //.writeTimeout(20, TimeUnit.SECONDS) //쓰는 기능은 현재 프로젝트에 없음.
+            //.addNetworkInterceptor(InterceptorForHeader()) 헤더가 필요 없음.
+            .build()
+
+        //리턴하는 레트로핏
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // SSL을 우회하는 OKHttpClient Builder를 생성한다. 모든 SSL 인증서를 허용하게 된다.
+    private fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        val sslSocketFactory = sslContext.socketFactory
+
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        builder.hostnameVerifier { hostname, session -> true }
+
+        return builder
     }
 }
