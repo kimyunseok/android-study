468b001cf591126f47ded25bbabc06ef2adcdda5
KHS-PC\pskna <psknal27@gmail.com>
2021-12-03 16:53:27 +0900

    fix. Repository 주석 추가

--- DesignPatternExampleProject/app/src/main/java/com/khs/designpatternexampleproject/ui/mvvm/repository/UserRepository.kt
+++ DesignPatternExampleProject/app/src/main/java/com/khs/designpatternexampleproject/ui/mvvm/repository/UserRepository.kt
@@ -2,6 +2,10 @@ package com.khs.designpatternexampleproject.ui.mvvm.repository
 
 import com.khs.designpatternexampleproject.model.UserModel
 
+/**
+ * Repository를 쓰면 데이터의 출처에 상관없이
+ * 상위 계층에서 일관성있는 데이터의 처리가 가능해진다.
+ */
 class UserRepository {
     val userModel = UserModel()
 
